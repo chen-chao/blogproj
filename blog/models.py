53f19f1bd9a822e161c997283d425fb3b268555c
chen-chao <wenbushi@gmail.com>
2018-06-18 13:39:00 +0800

    blog model: remove category, add postimage

--- blog/models.py
+++ blog/models.py
@@ -1,48 +1,31 @@
 # coding: utf-8
-import markdown
+from markdown2 import markdown
 from django.utils.html import strip_tags
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
 from django.utils.six import python_2_unicode_compatible
-
-
-@python_2_unicode_compatible
-class Category(models.Model):
-    """
-    https://docs.djangoproject.com/en/1.10/ref/models/fields/#field-types
-    """
-    name = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.name
-
-@python_2_unicode_compatible
-class Tag(models.Model):
-
-    name = models.CharField(max_length=100)
-
-    def __str__(self):
-        return self.name
+from django.core.files.base import ContentFile
+from taggit.managers import TaggableManager
 
 
 @python_2_unicode_compatible
 class Post(models.Model):
     """Post"""
     title = models.CharField(max_length=70)
-
     body = models.TextField()
+    html_file = models.FileField(upload_to='source/%Y/', blank=True)
 
-    created_time = models.DateTimeField()
-    modified_time = models.DateTimeField()
+    created_time = models.DateTimeField('date published', auto_now_add=True)
+    modified_time = models.DateTimeField('date edited', auto_now_add=True)
 
     # 文章摘要
     excerpect = models.CharField(max_length=200, blank=True)
 
-    category = models.ForeignKey(Category)
-    tags = models.ManyToManyField(Tag, blank=True)
+    tags = TaggableManager()
 
-    author = models.ForeignKey(User)
+    # keep posts when user deleted
+    author = models.ForeignKey(User, on_delete=models.DO_NOTHING)
 
     count_read = models.PositiveIntegerField(default=0)
 
@@ -56,15 +39,34 @@ class Post(models.Model):
         self.count_read += 1
         self.save(update_fields=['count_read'])
 
+    # overwrite models.save
     def save(self, *args, **kwargs):
-        if not self.excerpect:
-            md = markdown.Markdown(extensions=[
-                'markdown.extensions.extra',
-                'markdown.extensions.codehilite',
-            ])
-            self.excerpect = strip_tags(md.convert(self.body))[:32]
+        # TODO: add support to .rst .org .html
+        if not self.html_file:
+            html = markdown(self.body, extras=['fenced-code-blocks', 'tables'])
+            if not self.excerpect:
+                # TODO: header included
+                self.excerpect = strip_tags(html)[:100]
+            self.html_file.save(self.title + '.html',
+                                ContentFile(html.encode('utf-8')), save=False)
+            self.html_file.close()
+        super().save(*args, **kwargs)
 
-        super(Post, self).save(*args, **kwargs)
+    # delte html file when post deleted
+    def delete(self, *args, **kwargs):
+        if self.html_file:
+            self.html_file.delete(save=False)
+        super().delete(*args, **kwargs)
+
+    def display(self):
+        with open(self.html_file.path, encoding='utf-8') as f:
+            return f.read()
 
     class Meta:
-        ordering = ['-created_time', 'title']
+        ordering = ['-created_time', '-modified_time']
+
+
+class PostImage(models.Model):
+    post = models.ForeignKey(Post, related_name='images',
+                             on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='images/')
